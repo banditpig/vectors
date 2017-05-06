895029335b8acc349fb6744ddc3d3be3cbe82668
mike-k-houghton <mike_k_houghton@yahoo.co.uk>
2017-05-06 11:28:42 +0100

    functions that were on XYZ now on Vector  - gives cleaner code. Also using newtype  rather than data for Vector.

--- Vectors.hs
+++ Vectors.hs
@@ -7,18 +7,16 @@ module Vectors where
 type Scalar = Double
 type XYZ = (Scalar, Scalar, Scalar)
 
+newtype Vector = V { xyz :: XYZ } deriving ( Show)
 
-data Vector = V { xyz :: XYZ } deriving ( Show)
+mapVec :: (Scalar -> Scalar) -> Vector -> Vector
+mapVec f (V (x, y, z))  = V (f x, f y, f z)
 
--- functions on XYZ - maybe consider haing these work of Vector rather than the content, XYZ, of a Vector?
-mapXYZ :: (Scalar -> Scalar) -> XYZ -> XYZ
-mapXYZ f (x, y, z) = (f x, f y, f z)
+sumVec :: Vector -> Scalar
+sumVec (V (x, y, z)) = x + y + z
 
-sumXYZ :: XYZ -> Scalar
-sumXYZ (x, y, z) = x + y + z
-
-zipWithXYZ :: (Scalar -> Scalar -> Scalar) -> XYZ -> XYZ -> XYZ
-zipWithXYZ f (x, y, z) (x', y', z') =  (f x x', f y y', f z z')
+zipWithVec :: (Scalar -> Scalar -> Scalar) -> Vector -> Vector -> Vector
+zipWithVec f (V (x, y, z))  (V (x', y', z')) =   V (f x x', f y y', f z z')
 
 i, j, k:: Vector
 i = V (1, 0, 0)
@@ -26,35 +24,32 @@ j = V (0, 1, 0)
 k = V (0, 0, 1)
 
 vAdd :: Vector -> Vector -> Vector
-vAdd (V x) (V x') = V $ zipWithXYZ (+) x x'
+vAdd = zipWithVec (+) 
 
 vSub :: Vector -> Vector -> Vector
-vSub (V x) (V x') = V $ zipWithXYZ (-) x x'
+vSub  = zipWithVec (-)
 
 sMul :: Scalar -> Vector -> Vector
-sMul s (V x) = V $ mapXYZ (+s) x
+sMul s  = mapVec (+s)
 
 sMul' ::  Vector -> Scalar -> Vector
 sMul' = flip sMul
 
 sDiv :: Scalar -> Vector -> Vector
-sDiv s (V x) = V $ mapXYZ (/s) x
+sDiv s  = mapVec (/s) 
 
 dot :: Vector -> Vector -> Scalar
-dot (V x) (V x') = sumXYZ . zipWithXYZ (*) x $  x'
+dot x = sumVec . zipWithVec (*) x 
 
 cross :: Vector -> Vector -> Vector
 cross (V (u1, u2, u3)) (V (v1, v2, v3)) = V (u2*v3 - u3*v2, u3*v1 - u1*v3, u1*v2 - u2*v1)
 
 mag :: Vector -> Scalar
-mag (V x) = sqrt . sumXYZ . mapXYZ (^2) $ x
+mag  = sqrt . sumVec . mapVec (^2) 
 
 instance Monoid Vector where
     mempty = V (0, 0, 0)
     mappend = vAdd
-    -- mappend (V x) (V y) = V (x + x') (y + y') (z + z')
 
- 
-main :: IO ()
-main = putStrLn "Hello vector"
+
 
