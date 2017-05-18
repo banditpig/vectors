7d1850bf2da21c0c218acb8a176d676850ee93ee
banditpig <mike_k_houghton@yahoo.co.uk>
2017-05-18 22:21:27 +0100

    minor change

--- src/Vectors.hs
+++ src/Vectors.hs
@@ -19,13 +19,25 @@ module Vectors
   , normalise
   , neg
   , mag
-  ) where
+  , rotateXY
 
-type Scalar = Double
+  ) where
+import Text.Printf
+
+type Scalar = Float
 
 type XYZ = (Scalar, Scalar, Scalar)
 
-newtype Vector = V { xyz :: XYZ} deriving (Show, Eq)
+newtype Vector = V { xyz :: XYZ} deriving  (Eq)
+instance Show Vector where
+  show (V (x, y, z) ) = "(x:" ++ (p x) ++  " y:" ++ (p y) ++ " z:" ++ (p z) ++ ")"where 
+   p d = printf "%.2f" d :: String
+
+instance Monoid Vector where
+  mempty = origin
+  -- mappend :: Vector -> Vector -> Vector
+  mappend = (^+^)
+
 
 -- Define operators using  functions defines later
 infixl 6 ^+^
@@ -45,7 +57,7 @@ infixl 7 ^*
 (^*) = sMul'
 
 infixl 7 ^/
-(^/) :: Scalar -> Vector -> Vector
+(^/) :: Vector -> Scalar  -> Vector
 (^/) = sDiv
 
 infixl 7 >.<
@@ -66,7 +78,7 @@ origin = V (0, 0, 0)
 -- Normalise 
 normalise :: Vector -> Vector
 normalise (V (0, 0, 0)) = V (0, 0, 0) 
-normalise v = sDiv (mag v) v
+normalise v = sDiv v (mag v) 
 
 -- negate vector
 neg :: Vector -> Vector
@@ -76,6 +88,11 @@ neg = mapVec ((-1) *)
 mag :: Vector -> Scalar
 mag = sqrt . sumVec . mapVec (^2)
 
+-- rotate in the X Y plane.
+rotateXY :: Scalar -> Vector -> Vector
+rotateXY theta (V (x, y, z ))  = V (x', y', z) where
+  x' = x * cos theta - y * sin theta
+  y' = x * sin theta + y * cos theta
 -- ----------------------------------------------------------
 -- Map a function over the internal tuple
 mapVec :: (Scalar -> Scalar) -> Vector -> Vector
@@ -108,8 +125,8 @@ sMul s = mapVec (* s)
 sMul' :: Vector -> Scalar -> Vector
 sMul' = flip sMul
 
-sDiv :: Scalar -> Vector -> Vector
-sDiv s = mapVec (/ s)
+sDiv :: Vector -> Scalar ->  Vector
+sDiv v s = mapVec (/ s) v
 
 -- dot product
 dot :: Vector -> Vector -> Scalar
