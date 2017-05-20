9e1c202479fdddc0f637122e0adba4104c32e9f0
banditpig <mike_k_houghton@yahoo.co.uk>
2017-05-20 15:22:04 +0100

    tidy and minor change in arrowHead

--- src/Views.hs
+++ src/Views.hs
@@ -7,33 +7,39 @@ import Data.Monoid
 
 
 lineVector :: V.Vector -> V.Vector -> Picture
-lineVector  v1@(V (xc, yc, _)) v2@(V (x, y, _)) = Line [(xc, yc),  (x, y)]  <> arrowHead v1 v2
+lineVector  pv@(V (xp, yc, _)) v@(V (x, y, _)) 
+   = Line 
+     [(xp, yc),  (x, y)]  
+     <> arrowHead pv v
 
 lineVectorO ::  V.Vector -> Picture
-lineVectorO = lineVector V.origin
+lineVectorO = lineVector V.origin 
 
 arrowHead :: V.Vector -> V.Vector -> Picture 
-arrowHead v1 v2 = polygonFromVectors [v2, v2 ^+^ v', v2 ^+^ v''] where 
-    v   = normalise (v1 ^-^ v2) 
-    v'  = 10 *^ rotateXY (pi/8) v
-    v'' = 10 *^ rotateXY ( (-1)*pi/8) v
+arrowHead pv v = polygonFromVectors [v, v ^+^ v', v ^+^ v''] where 
+    vdiff   = neg . normalise $ v ^-^ pv
+    v'      = 10 *^ rotateXY (pi/8) vdiff
+    v''     = 10 *^ rotateXY ( (-1)*pi/8) vdiff
 
 polygonFromVectors :: [V.Vector] -> Picture
 polygonFromVectors vs = Polygon pts where
     pts = map f vs where
         f (V (x, y, _) ) = (x, y)
 
+drawIt :: [Picture] -> IO ()
+drawIt ps = display window background (axes <> mconcat ps) 
 
 window :: Display
 window = InWindow "Window" (1400, 800) (10, 10)
 
 background :: Color
-background = white
+background = greyN 0.7
 
 axes :: Picture
 axes = color red (line [ (-10000, 0), (10000,  0) ]) <>
        color red (line [ (0, -10000), (0,  10000) ])
- 
-drawIt :: [Picture] -> IO ()
-drawIt ps = display window background (axes <> mconcat ps) 
+      
+
+-- main :: IO ()
+-- main = display window background axes 
 
